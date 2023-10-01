bd6bdca7d6c21b43ca5d35966a194e20b127a8b3
Maksim Klimov <maksimklimov779@gmail.com>
2023-10-01 20:21:10 +0300

    Добавлен класс ShiftCoordinate.

---
+++ src/main/java/ru/klimov/simulation/ShiftCoordinate.java
@@ -0,0 +1,11 @@
+package ru.klimov.simulation;
+
+public class ShiftCoordinate {
+    int shiftRow;
+    int shiftColumn;
+
+    public ShiftCoordinate(int shiftRow, int shiftColumn) {
+        this.shiftRow = shiftRow;
+        this.shiftColumn = shiftColumn;
+    }
+}
